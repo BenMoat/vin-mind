f01ffc21df9c395ee3751a22799fcb0f527631f5
Ben Moat <benmoat@hotmail.co.uk>
2023-10-22 17:28:10 +0100

    Re-added the loading skeletons and added a retry button to a failed instance

--- components/tax-and-mot.tsx
+++ components/tax-and-mot.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import axios from "axios";
 import { CheckCircle, XCircle } from "lucide-react";
 import {
@@ -13,6 +13,8 @@ import {
 } from "./ui/card";
 import { DvlaData } from "@prisma/client";
 import { LastUpdatedBadge } from "./last-updated-badge";
+import CardSkeleton from "./card-skeleton";
+import { Button } from "./ui/button";
 
 interface DvlaDataProps {
   initialData: DvlaData;
@@ -20,8 +22,8 @@ interface DvlaDataProps {
 
 export const TaxAndMOT: React.FC<DvlaDataProps> = ({ initialData }) => {
   const params = useParams();
-  const router = useRouter();
 
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const registrationNumber = initialData.registrationNumber;
@@ -33,6 +35,8 @@ export const TaxAndMOT: React.FC<DvlaDataProps> = ({ initialData }) => {
   const fetchData = async () => {
     try {
       if (hoursSinceUpdate < 24) {
+        setLoading(true);
+        setError("");
         const response = await axios.post(
           `/api/${params.vehicleId}/vehicle-enquiry`,
           { registrationNumber }
@@ -41,30 +45,39 @@ export const TaxAndMOT: React.FC<DvlaDataProps> = ({ initialData }) => {
       }
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        setError(error.response.data.message);
+        setError("Failed to fetch data from the DVLA.");
       }
+    } finally {
+      setLoading(false);
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   async function saveData(data: DvlaDataProps) {
     try {
+      setLoading(true);
+      setError("");
       if (initialData) {
         await axios.patch(
           `/api/${params.vehicleId}/vehicle-enquiry/save-enquiry`,
           data
         );
       }
-      router.refresh();
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        setError(error.response.data.message);
+        setError("Failed to sync DVLA data with database.");
       }
+    } finally {
+      setLoading(false);
     }
   }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleRetry = () => {
+    fetchData();
+  };
 
   return (
     <>
@@ -75,86 +88,100 @@ export const TaxAndMOT: React.FC<DvlaDataProps> = ({ initialData }) => {
               <XCircle className="w-8 h-8 text-red-500" />
             </span>
             <p className="text-sm">{error}</p>
+            <Button variant="outline" onClick={handleRetry}>
+              Retry
+            </Button>
           </div>
         </div>
-      ) : (
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {initialData.taxStatus === "Taxed" ? (
-            <Card className="relative px-6 space-y-2 text-center">
-              <LastUpdatedBadge tax updatedAt={initialData.updatedAt} />
-              <CardHeader>
-                <CardTitle>Tax</CardTitle>
-                <CardDescription>
-                  <span className="flex items-center justify-center">
-                    <CheckCircle className="w-8 h-8 mr-2 text-[#adfa1d]" />
-                    {initialData.taxStatus}
-                  </span>
-                </CardDescription>
-                <CardContent className="pb-0">
-                  Due:{" "}
-                  {new Date(initialData.taxDueDate).toLocaleDateString("en-GB")}
-                </CardContent>
-              </CardHeader>
-            </Card>
-          ) : (
-            <Card className="relative px-6 space-y-2 text-center">
-              <LastUpdatedBadge tax updatedAt={initialData.updatedAt} />
-              <CardHeader>
-                <CardTitle>Tax</CardTitle>
-                <CardDescription>
-                  <span className="flex items-center justify-center">
-                    <XCircle className="w-8 h-8 mr-2 text-[#7f1d1d]" />
-                    {initialData.taxStatus}
-                  </span>
-                </CardDescription>
-                <CardContent className="pb-0">
-                  Due:{" "}
-                  {new Date(initialData.taxDueDate).toLocaleDateString("en-GB")}
-                </CardContent>
-              </CardHeader>
-            </Card>
-          )}
-
-          {initialData.motStatus === "Valid" ? (
-            <Card className="relative px-6 space-y-2 text-center">
-              <LastUpdatedBadge mot updatedAt={initialData.updatedAt} />
-              <CardHeader>
-                <CardTitle>MOT</CardTitle>
-                <CardDescription>
-                  <span className="flex items-center justify-center">
-                    <CheckCircle className="w-8 h-8 mr-2 text-[#adfa1d]" />
-                    {initialData.motStatus}
-                  </span>
-                </CardDescription>
-                <CardContent className="pb-0">
-                  Expires:{" "}
-                  {new Date(initialData.motExpiryDate).toLocaleDateString(
-                    "en-GB"
-                  )}
-                </CardContent>
-              </CardHeader>
-            </Card>
-          ) : (
-            <Card className="relative px-6 space-y-2 text-center">
-              <LastUpdatedBadge mot updatedAt={initialData.updatedAt} />
-              <CardHeader>
-                <CardTitle>MOT</CardTitle>
-                <CardDescription>
-                  <span className="flex items-center justify-center">
-                    <XCircle className="w-8 h-8 mr-2 text-[#7f1d1d]" />
-                    {initialData.motStatus}
-                  </span>
-                </CardDescription>
-                <CardContent className="pb-0">
-                  Expires:{" "}
-                  {new Date(initialData.motExpiryDate).toLocaleDateString(
-                    "en-GB"
-                  )}
-                </CardContent>
-              </CardHeader>
-            </Card>
-          )}
+      ) : loading ? (
+        <div className="grid gap-4 grid-cols-3">
+          <CardSkeleton />
+          <CardSkeleton />
         </div>
+      ) : (
+        initialData && (
+          <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {initialData.taxStatus === "Taxed" ? (
+              <Card className="relative px-6 space-y-2 text-center">
+                <LastUpdatedBadge tax updatedAt={initialData.updatedAt} />
+                <CardHeader>
+                  <CardTitle>Tax</CardTitle>
+                  <CardDescription>
+                    <span className="flex items-center justify-center">
+                      <CheckCircle className="w-8 h-8 mr-2 text-[#adfa1d]" />
+                      {initialData.taxStatus}
+                    </span>
+                  </CardDescription>
+                  <CardContent className="pb-0">
+                    Due:{" "}
+                    {new Date(initialData.taxDueDate).toLocaleDateString(
+                      "en-GB"
+                    )}
+                  </CardContent>
+                </CardHeader>
+              </Card>
+            ) : (
+              <Card className="relative px-6 space-y-2 text-center">
+                <LastUpdatedBadge tax updatedAt={initialData.updatedAt} />
+                <CardHeader>
+                  <CardTitle>Tax</CardTitle>
+                  <CardDescription>
+                    <span className="flex items-center justify-center">
+                      <XCircle className="w-8 h-8 mr-2 text-[#7f1d1d]" />
+                      {initialData.taxStatus}
+                    </span>
+                  </CardDescription>
+                  <CardContent className="pb-0">
+                    Due:{" "}
+                    {new Date(initialData.taxDueDate).toLocaleDateString(
+                      "en-GB"
+                    )}
+                  </CardContent>
+                </CardHeader>
+              </Card>
+            )}
+
+            {initialData.motStatus === "Valid" ? (
+              <Card className="relative px-6 space-y-2 text-center">
+                <LastUpdatedBadge mot updatedAt={initialData.updatedAt} />
+                <CardHeader>
+                  <CardTitle>MOT</CardTitle>
+                  <CardDescription>
+                    <span className="flex items-center justify-center">
+                      <CheckCircle className="w-8 h-8 mr-2 text-[#adfa1d]" />
+                      {initialData.motStatus}
+                    </span>
+                  </CardDescription>
+                  <CardContent className="pb-0">
+                    Expires:{" "}
+                    {new Date(initialData.motExpiryDate).toLocaleDateString(
+                      "en-GB"
+                    )}
+                  </CardContent>
+                </CardHeader>
+              </Card>
+            ) : (
+              <Card className="relative px-6 space-y-2 text-center">
+                <LastUpdatedBadge mot updatedAt={initialData.updatedAt} />
+                <CardHeader>
+                  <CardTitle>MOT</CardTitle>
+                  <CardDescription>
+                    <span className="flex items-center justify-center">
+                      <XCircle className="w-8 h-8 mr-2 text-[#7f1d1d]" />
+                      {initialData.motStatus}
+                    </span>
+                  </CardDescription>
+                  <CardContent className="pb-0">
+                    Expires:{" "}
+                    {new Date(initialData.motExpiryDate).toLocaleDateString(
+                      "en-GB"
+                    )}
+                  </CardContent>
+                </CardHeader>
+              </Card>
+            )}
+          </div>
+        )
       )}
     </>
   );
