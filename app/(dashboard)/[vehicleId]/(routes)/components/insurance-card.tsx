8a9154f1ecc95c15d89af7f57ec2dd5946fe60b8
Ben Moat <benmoat@hotmail.co.uk>
2023-10-31 20:24:40 +0000

    - Insurance status card will only update every 24 hours under the same paramaters as the tax and MOT

--- app/(dashboard)/[vehicleId]/(routes)/components/insurance-card.tsx
+++ app/(dashboard)/[vehicleId]/(routes)/components/insurance-card.tsx
@@ -58,7 +58,17 @@ export const InsuranceCard: React.FC<InsuranceCardProps> = ({
         setLoading(false);
       }
     };
-    fetchData();
+
+    /* Cache a timestamp for when the data was last fetched. Compare it against the current time
+    to determine whether to fetch new data or not.*/
+    const storedTimestamp = parseInt(localStorage.getItem("insTMS") || "0", 10);
+    const currentTime = Date.now();
+    const timeThreshold = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+    if (currentTime - storedTimestamp > timeThreshold) {
+      fetchData();
+      localStorage.setItem("insTMS", currentTime.toString());
+    }
   }, [initialData]);
 
   async function saveData(data: Insurance) {
