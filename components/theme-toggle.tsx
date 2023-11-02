92b833e89cb5f47089148dbc8e534731db33e250
Ben Moat <benmoat@hotmail.co.uk>
2023-11-02 20:51:25 +0000

    The theme toggle component now retains the background accent of the selected item, then removes it upon hovering over other items like select lists do

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -16,12 +16,14 @@ import {
 
 export function ThemeToggle() {
   const { setTheme, resolvedTheme } = useTheme();
+  const [isHovering, setIsHovering] = React.useState(false);
+
   const defaultMode =
     typeof window !== "undefined" &&
     window.matchMedia("(prefers-color-scheme: dark)").matches;
 
   return (
-    <DropdownMenu>
+    <DropdownMenu onOpenChange={() => setIsHovering(false)}>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
@@ -31,22 +33,38 @@ export function ThemeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuCheckboxItem
-          className="cursor-pointer"
+          className={`cursor-pointer ${
+            resolvedTheme === "light" && !isHovering
+              ? "bg-accent"
+              : "hover:bg-accent"
+          }`}
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
           checked={resolvedTheme === "light"}
           onClick={() => setTheme("light")}
         >
+          <Sun className="absolute left-2 h-4 w-4 mr-2" />
           Light
         </DropdownMenuCheckboxItem>
         <DropdownMenuCheckboxItem
-          className="cursor-pointer"
+          className={`cursor-pointer ${
+            resolvedTheme === "dark" && !isHovering
+              ? "bg-accent"
+              : "hover:bg-accent"
+          }`}
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
           checked={resolvedTheme === "dark"}
           onClick={() => setTheme("dark")}
         >
+          <Moon className="absolute left-2 h-4 w-4 mr-2" />
           Dark
         </DropdownMenuCheckboxItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
-          className="cursor-pointer flex justify-center space-x-2"
+          className="cursor-pointer flex justify-center space-x-2 hover:bg-accent"
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
           onClick={() => setTheme("system")}
         >
           System
