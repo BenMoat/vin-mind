b9b37d09e0fd299f5dfe6cc03d2fb339d28d3a9a
Ben Moat <benmoat@hotmail.co.uk>
2023-11-02 20:23:29 +0000

    - Overhauled the dark mode colours - Added the VinMind logo text to the main nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 import { UserButton, auth } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
 
+import { Gauge } from "lucide-react";
+
 import { MainNav } from "@/components/main-nav";
 import VehicleSwitcher from "@/components/vehicle-switcher";
 import { ThemeToggle } from "@/components/theme-toggle";
@@ -21,7 +23,9 @@ export const Navbar = async () => {
 
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4">
+      <div className="flex h-16 items-center px-4 cursor-default">
+        <Gauge className="ml-4 w-5 h-5" />
+        <p className="text-xl font-bold ml-2 mr-6">VinMind</p>
         <VehicleSwitcher items={vehicles} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
