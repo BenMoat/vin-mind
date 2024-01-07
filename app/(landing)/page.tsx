6f4ddd62c6510ac1868ff34e459062a2cbc56992
Ben Moat <benmoat@hotmail.co.uk>
2024-01-07 17:23:52 +0000

    Updated the images and replaced the carousel with a display of images

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-import InfoCards from "@/components/info-cards";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import InfoCards from "@/components/info-cards";
 
-export default function asd() {
+export default function LandingPage() {
   const images = [
     "/dashboard.png",
     "/modifications.png",
@@ -30,7 +16,7 @@ export default function asd() {
   ];
 
   const imageDescriptions = [
-    "Fully populated dashboard",
+    "Your dashboard provides an overview of your vehicle. It includes your tax, MOT, and insurance status, as well as your vehicle's details.",
     "Table of modifications",
     "Servicing history, including duration and mileage between services",
     "Switch between various vehicles",
@@ -38,64 +24,61 @@ export default function asd() {
   ];
 
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-      <div className="container px-4 md:px-6">
-        <div className="grid gap-6 items-center">
-          <div className="flex flex-col justify-center space-y-8 text-center">
-            <div className="space-y-6">
-              <h1 className="text-3xl font-bold sm:text-6xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-blue-700">
-                VinMind
-              </h1>
-              <p className="text-xl">Your multi-vehicle management solution.</p>
-              <div className="space-x-4">
-                <Link href="/sign-up">
-                  <Button>Get Started</Button>
-                </Link>
-                <Link href="/sign-in">
-                  <Button variant="outline">Log in</Button>
-                </Link>
+    <>
+      <section className="w-full">
+        <div className="container px-4 md:px-6">
+          <div className="grid gap-6 items-center">
+            <div className="flex flex-col justify-center space-y-8 text-center">
+              <div className="space-y-6 pt-20">
+                <h1 className="text-3xl font-bold sm:text-6xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-blue-900">
+                  VinMind
+                </h1>
+                <p className="text-xl">
+                  Your multi-vehicle management solution.
+                </p>
+                <div className="space-x-4">
+                  <Link href="/sign-up">
+                    <Button>Get Started</Button>
+                  </Link>
+                  <Link href="/sign-in">
+                    <Button variant="outline">Log in</Button>
+                  </Link>
+                </div>
               </div>
+              <InfoCards />
             </div>
-            <InfoCards />
-
-            <Carousel
-              opts={{
-                align: "start",
-                loop: true,
-              }}
-              plugins={[
-                Autoplay({
-                  delay: 2000,
-                  stopOnInteraction: false,
-                }),
-              ]}
-              className="w-full"
-            >
-              <CarouselContent className="">
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <CarouselItem
-                    key={index}
-                    className="md:basis-1/2 lg:basis-1/2"
-                  >
-                    <Card>
-                      <CardHeader className="flex justify-center max-h-12 pb-2 pt-2">
-                        {imageDescriptions[index]}
-                      </CardHeader>
-
-                      <CardContent className="flex aspect-square items-center justify-center p-1 border-t rounded-lg">
-                        <img
-                          src={images[index]}
-                          alt={`Carousel item ${index + 1}`}
-                        />
-                      </CardContent>
-                    </Card>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-            </Carousel>
           </div>
         </div>
-      </div>
-    </section>
+        <div className="bg-gradient-to-t from-gray-900 to-black min-h-[220px] mt-[-40px] flex items-center justify-center">
+          <h2 className="text-4xl font-bold text-center">Features</h2>
+        </div>
+      </section>
+      {images.map((image, index) => (
+        <section key={index} className="w-full bg-gray-900">
+          <div
+            className={`flex flex-wrap ${
+              index % 2 === 0 ? "flex-row-reverse" : ""
+            } container mx-auto`}
+          >
+            <div className="w-full md:w-1/2 flex flex-col justify-center items-start pl-10 py-5">
+              <p className="text-xl text-white">{imageDescriptions[index]}</p>
+            </div>
+            <div className="w-full md:w-1/2">
+              <div className="relative pb-1">
+                <Image
+                  src={image}
+                  layout="responsive"
+                  width={400}
+                  height={400}
+                  objectFit="fill"
+                  alt={`Image ${index + 1}`}
+                  className="rounded-lg shadow-lg shadow-black"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      ))}
+    </>
   );
 }
