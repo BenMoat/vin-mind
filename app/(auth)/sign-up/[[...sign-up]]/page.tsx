dab4f3983c3e11b9afb5619a84887f4564281d99
Ben Moat <benmoat@hotmail.co.uk>
2024-01-15 22:41:59 +0000

    Clerk sign in and sign up screens are now in dark mode

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
-  return <SignUp afterSignUpUrl="/get-started" />;
+  return (
+    <SignUp afterSignUpUrl="/get-started" appearance={{ baseTheme: dark }} />
+  );
 }
