3e860d2a5096c0abbc0b98a71b51b0aeffc441f6
Ben Moat <benmoat@hotmail.co.uk>
2024-01-23 22:45:08 +0000

    BREAKING CHANGES: - Dynamic route of [vehicleId] has been replaced with [vehicleSlug]. Only top level navigational options work right now. - User table has been created. - All database changes are done on dev branch of PlanetScale.

--- lib/util-types/slug-utils.ts
+++ lib/util-types/slug-utils.ts
@@ -1,6 +1,3 @@
 export default function generateSlug(name: string): string {
-  return name
-    .toLowerCase() // Convert to lower case
-    .trim() // Remove leading and trailing whitespace
-    .replace(/\s+/g, "-"); // Replace spaces with hyphens
+  return name.toLowerCase().trim().replace(/\s+/g, "-");
 }
